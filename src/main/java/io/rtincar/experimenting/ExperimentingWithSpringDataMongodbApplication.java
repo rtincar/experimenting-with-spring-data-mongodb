ed5f546356e109a66bc80ca8880e0382557988b9
rtincar <ul2mac@gmail.com>
2017-11-22 22:05:50 +0100

    Inicio proyecto

---
+++ src/main/java/io/rtincar/experimenting/ExperimentingWithSpringDataMongodbApplication.java
@@ -0,0 +1,12 @@
+package io.rtincar.experimenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentingWithSpringDataMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperimentingWithSpringDataMongodbApplication.class, args);
+	}
+}
